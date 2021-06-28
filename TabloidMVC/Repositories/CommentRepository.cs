33a44fd2503bc840ca6c1ee6154109ff29572141
ColtMay <colten_mayberry@pm.me>
2021-06-28 15:05:08 -0500

    added comment repository

---
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public class CommentRepository : BaseRepository, ICommentRepository
+    {
+        public CommentRepository(IConfiguration config) : base(config) { }
+
+        public List<Comment> GetCommentsByPost(int PostId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
