f876b1dbe45b2299bb2f8b37d84eb95f3490dbfc
Bryson Goins <brysonmg@gmail.com>
2021-07-01 09:57:54 -0500

    view subscriptions on home page - ready to test

--- TabloidMVC/Controllers/HomeController.cs
+++ TabloidMVC/Controllers/HomeController.cs
@@ -2,25 +2,44 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using TabloidMVC.Models;
+using TabloidMVC.Repositories;
 
 namespace TabloidMVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IPostRepository _postRepo;
 
-        public HomeController(ILogger<HomeController> logger)
+        public int GetCurrentUserId()
+        {
+            string id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (id == null)
+            {
+                id = "0";
+            }
+            return int.Parse(id);
+        }
+
+        public HomeController(ILogger<HomeController> logger, IPostRepository postRepository)
         {
             _logger = logger;
+            _postRepo = postRepository;
         }
 
         public IActionResult Index()
         {
-            return View();
+            if (GetCurrentUserId() == 0)
+            {
+                return View(new List<Post>());
+            }
+            List<Post> subPosts = _postRepo.GetSubbedPosts(GetCurrentUserId());
+            return View(subPosts);
         }
 
         public IActionResult Privacy()
